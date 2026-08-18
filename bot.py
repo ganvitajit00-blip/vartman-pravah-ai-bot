9a2b893f2b265ee2956002a9d43a121c6cc3ea42
ganvitajit00-blip <ganvitajit00@gmail.com>
2026-08-18 18:46:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,16 @@ from telegram.ext import (
 )
 
 # -----------------------------
-# SETTINGS
+# server = Flask(__name__)
+
+@server.route('/')
+def home():
+    return "Bot is Live!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 8080))
+    server.run(host="0.0.0.0", port=port)
+
 # -----------------------------
 
 TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
